0b942fd26e9a4160b49aad97c6e705cf3da092eb
fratur <francescoturco2795@gmail.com>
2025-05-19 20:01:08 +0200

    exercise-done

--- paridispari/paridispari.js
+++ paridispari/paridispari.js
@@ -9,3 +9,48 @@ Dichiariamo chi ha vinto.*/
 
 
 
+// chiedo all'utente di scegliere un numero tra pari e dispari 
+let sceltaUtente = prompt("Scegli pari o dispari");
+sceltaUtente = sceltaUtente.toLowerCase(); // trasformo tutto in minuscolo per non avere conflitti con caratteri maiusc/minusc
+console.log(sceltaUtente)
+
+
+// chiedo all'utente un numero da 1 a 5
+let numeroUtente = parseInt(prompt("Scrivi un numero da 1 a 5"));
+console.log('numero utente', numeroUtente);
+
+// funzione che genera un numero a caso da 1 a 5 
+function generaNumeroRandom() {
+  let numeroCasuale = Math.floor(Math.random() * 5) + 1;
+  console.log('nuemro computer', numeroCasuale);
+  return numeroCasuale;
+}
+
+// usoo la funzione per far scegliere un numero al computer
+let numeroComputer = generaNumeroRandom();
+
+// sommo i due numeri utente e computer
+let somma = numeroUtente + numeroComputer;
+console.log('somma numero utente vs computer', somma)
+
+// uso un'altra funzione per sapere se un numero è pari o dispari
+function verificaPariDispari(numero) {
+  if (numero % 2 === 0) {
+    return "pari";
+  } else {
+    return "dispari";
+  }
+}
+
+
+// con una variabile controllo se l'utente ha vinto o ha perso
+let risultato = verificaPariDispari(somma);
+console.log(risultato);
+
+
+if (sceltaUtente === risultato) {
+  console.log("Hai vinto!");
+} else {
+  console.log("Hai perso!");
+}
+
